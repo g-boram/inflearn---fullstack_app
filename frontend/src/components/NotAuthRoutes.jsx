a24b5ab0e4b48c8d67e25ff16e20fd7de5f278f4
GOBORAM <109337202+g-boram@users.noreply.github.com>
2023-10-01 13:33:22 +0900

    first-commit

---
+++ frontend/src/components/NotAuthRoutes.jsx
@@ -0,0 +1,11 @@
+// 이미 로그인된 사람이 로그인이나,회원가입 페이지에 들어가지못하도록 리다이렉트
+
+const NotAuthRoutes = () => {
+  return (
+    <div>
+      NotAuthRoutes
+    </div>
+  )
+}
+
+export default NotAuthRoutes
---
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,11 @@
+// 로그인이 되지않은사람이 로그인된사람만 볼수있는 페이지로 갔을때 리다이렉트
+
+const ProtectedRoutes = () => {
+  return (
+    <div>
+      ProtectedRoutes
+    </div>
+  )
+}
+
+export default ProtectedRoutes
