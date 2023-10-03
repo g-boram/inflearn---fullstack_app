971c65226c2f99d29b1c3cb9f4b73f68c2c11a9e
GOBORAM <109337202+g-boram@users.noreply.github.com>
2023-10-03 16:27:29 +0900

    회원가입-UI작성

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,32 +1,31 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer } from "redux-persist";
 import userReducer from './userSlice';
 import storage from 'redux-persist/lib/storage';
-import persistStore from "redux-persist/es/persistStore";
-
-
+import {
+    FLUSH, PAUSE, PERSIST, PURGE, REGISTER,
+    REHYDRATE, persistReducer, persistStore
+} from 'redux-persist';
 
 const rootReducer = combineReducers({
-  user: userReducer,
+    user: userReducer,
 })
 
 const persistConfig = {
-  key: 'root',
-  storage
+    key: 'root',
+    storage
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: persistedReducer ,
-    middleware: getDefaultMiddleware => getDefaultMiddleware({
-      serializableCheck: {
-        
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      }
-    })
-  }
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware(
+        {
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+            }
+        }
+    )
 })
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
